08b4a4b67757b7fc749d1ce73d27d85ec3b1b371
vankokos <vano9995@gmail.com>
2022-03-26 18:16:08 +0100

    done

---
+++ js/slider.js
@@ -0,0 +1,9 @@
+$('.info__inner-reviews-bottom-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    fade: true,
+    cssEase: 'linear',
+    arrows: true,
+    autoplay: true,
+  });
